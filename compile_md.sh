f53c88ae9d0ecdbbbba57598ac3d0c7a6dc307c7
Tom Pollak <tompollak1000@gmail.com>
2024-10-09 09:43:08 +0100

    added nbs

--- compile_md.sh
+++ compile_md.sh
@@ -1,11 +1,14 @@
 echo "*****"
 mkdir -p pages
-for file in md/*.md; do
-  filename=$(basename "$file" .md)
+
+# Process both markdown and Jupyter notebook files
+for file in md/*.md nbs/*.ipynb; do
+  filename=$(basename "$file" | cut -f 1 -d '.')
   pandoc "$file" -o "pages/$filename.html" \
     --template=markdown_template.html \
     --metadata title="$filename" \
     --mathjax
   echo "processed $filename"
 done
+
 echo "*****"
