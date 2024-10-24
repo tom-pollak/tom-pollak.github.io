0f4c56b467e0880cb158ad871972f2a902d4873f
Tom Pollak <tompollak1000@gmail.com>
2024-10-24 12:00:15 +0100

    update compile script

--- compile.sh
+++ compile.sh
@@ -3,9 +3,10 @@ echo "*****"
 mkdir -p pages
 
 for fp in md/*.md nbs/*.ipynb; do
-  filename=$(basename "$fp" | cut -f 1 -d '.')
+  fp_no_extension=$(echo "$fp" | cut -f 1 -d '.')
+  filename=$(basename "$fp_no_extension")
   quarto render "$fp" --to html --output-dir "./pages/" --no-execute
-  mv "./pages/$(echo "$fp" | cut -f 1 -d '.').html" "./pages/$filename.html"
+  mv "./pages/$fp_no_extension.html" "./pages/$filename.html"
   echo "processed $filename"
 done
 
