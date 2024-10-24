fa1b9e604bf640b1de530240c8d31a929719284b
Tom Pollak <tompollak1000@gmail.com>
2024-10-24 11:57:50 +0100

    removed nl

--- compile.sh
+++ compile.sh
@@ -5,9 +5,6 @@ mkdir -p pages
 for fp in md/*.md nbs/*.ipynb; do
   filename=$(basename "$fp" | cut -f 1 -d '.')
   quarto render "$fp" --to html --output-dir "./pages/" --no-execute
-
-  
-
   mv "./pages/$(echo "$fp" | cut -f 1 -d '.').html" "./pages/$filename.html"
   echo "processed $filename"
 done
