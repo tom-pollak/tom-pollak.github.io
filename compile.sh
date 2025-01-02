9a798efd6d9a1aa44681a6239da995e483a9e44f
Tom Pollak <tompollak1000@gmail.com>
2025-01-02 11:43:41 +0100

    optional execute

--- compile.sh
+++ compile.sh
@@ -5,7 +5,7 @@ mkdir -p pages
 for fp in md/*.md nbs/*.ipynb; do
   fp_no_extension=$(echo "$fp" | cut -f 1 -d '.')
   filename=$(basename "$fp_no_extension")
-  quarto render "$fp" --to html --output-dir "./pages/" --no-execute
+  quarto render "$fp" --to html --output-dir "./pages/"
   mv "./pages/$fp_no_extension.html" "./pages/$filename.html"
   echo "processed $filename"
 done
